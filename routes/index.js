9ab62b43d2895933fee5b450013aacca6825f20b
Martin Eley <meley@salesforce.com>
2022-12-12 15:55:57 +0000

    changed path to client

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 /* GET home page. */
 router.get('/loan', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  res.sendFile(path.join(__dirname, '/../../client/build/index.html'));
 });
 
 module.exports = router;
